20a15ed30e14bcd63dea090cc88fac4e42368877
t-cool <hiroki802@hotmail.com>
2022-02-01 14:57:21 +0900

    tweak

--- index.js
+++ index.js
@@ -175,9 +175,3 @@ const padZero3Digit = ( num ) => {
   }
 }
 
-
-// Show readme
-document.getElementById( "btShowReadMe" ).addEventListener( "click", async ev => {
-  window.open('https://github.com/tetunori/HTML5WebcamTester/blob/master/README.md','_blank');
-});
-
